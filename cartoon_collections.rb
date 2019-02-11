09079da285ec0644507cbd08cfc7098024a4c033
Andrew Kim <akim618@gmail.com>
2019-02-11 23:05:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)# code an argument here
-  planeteer_calls = []
+  planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
    array.collect do |calls|
      planeteer_calls << "#{calls.capitalize}!"
    end
